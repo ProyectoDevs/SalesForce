b974547464fc83ed06fc74aae965fe41ff072329
Andrés Fresneda <michael.fresneda@hotmail.com>
2022-08-23 13:40:05 -0500

    Class Empresa

---
+++ Empresa.java
@@ -0,0 +1,46 @@
+public class Empresa {
+    private String nombreEmpresa;
+    private String direccionEmpresa;
+    private int telefonoEmpresa;
+    private long  nitEmpresa;
+
+    public Empresa(String nombreEmpresa, String direccionEmpresa, int telefonoEmpresa, long nitEmpresa) {
+        this.nombreEmpresa = nombreEmpresa;
+        this.direccionEmpresa = direccionEmpresa;
+        this.telefonoEmpresa = telefonoEmpresa;
+        this.nitEmpresa = nitEmpresa;
+    }
+
+      public String getNombreEmpresa() {
+        return nombreEmpresa;
+    }
+
+    public void setNombreEmpresa(String nombreEmpresa) {
+        this.nombreEmpresa = nombreEmpresa;
+    }
+
+    public String getDireccionEmpresa() {
+        return direccionEmpresa;
+    }
+
+    public void setDireccionEmpresa(String direccionEmpresa) {
+        this.direccionEmpresa = direccionEmpresa;
+    }
+
+    public int getTelefonoEmpresa() {
+        return telefonoEmpresa;
+    }
+
+    public void setTelefonoEmpresa(int telefonoEmpresa) {
+        this.telefonoEmpresa = telefonoEmpresa;
+    }
+
+    public long getNitEmpresa() {
+        return nitEmpresa;
+    }
+
+    public void setNitEmpresa(int nitEmpresa) {
+        this.nitEmpresa = nitEmpresa;
+    }
+
+}
