93a2ee26272cdccf9b236c6fc831d37254549f67
LUISGA64 <luisgalic64@gmail.com>
2022-08-27 09:51:44 -0500

    creacion de clase MovimientoDinero

---
+++ salesForce/src/main/java/com/salesForce/model/MovimientoDinero.java
@@ -0,0 +1,37 @@
+package com.salesForce.model;
+
+public class MovimientoDinero {
+    private float montoMovimiento;
+    private String conceptoMovimiento;
+    private String usuarioMovimiento;
+
+    public MovimientoDinero(float montoMovimiento, String conceptoMovimiento, String usuarioMovimiento) {
+        this.montoMovimiento = montoMovimiento;
+        this.conceptoMovimiento = conceptoMovimiento;
+        this.usuarioMovimiento = usuarioMovimiento;
+    }
+
+    public float getMontoMovimiento() {
+        return montoMovimiento;
+    }
+
+    public void setMontoMovimiento(float montoMovimiento) {
+        this.montoMovimiento = montoMovimiento;
+    }
+
+    public String getConceptoMovimiento() {
+        return conceptoMovimiento;
+    }
+
+    public void setConceptoMovimiento(String conceptoMovimiento) {
+        this.conceptoMovimiento = conceptoMovimiento;
+    }
+
+    public String getUsuarioMovimiento() {
+        return usuarioMovimiento;
+    }
+
+    public void setUsuarioMovimiento(String usuarioMovimiento) {
+        this.usuarioMovimiento = usuarioMovimiento;
+    }
+}
