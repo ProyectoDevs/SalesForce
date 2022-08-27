b3a93acdcbfbadf35018583877c5a206a5c120b7
LUISGA64 <luisgalic64@gmail.com>
2022-08-27 07:34:43 -0500

    proyecto java SalesForce

---
+++ salesForce/src/main/java/com/salesForce/SalesForceApplication.java
@@ -0,0 +1,13 @@
+package com.salesForce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesForceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesForceApplication.class, args);
+	}
+
+}
