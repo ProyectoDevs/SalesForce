8cc30f15fc1c5158c5be4872926289c6486ca93d
LUISGA64 <luisgalic64@gmail.com>
2022-09-10 12:49:39 -0500

    Revert "hola buena noches, este es el controller."
    This reverts commit 291ac14d

--- src/main/java/com/salesForce/controller/EmpleadoController.java
+++ src/main/java/com/salesForce/controller/EmpleadoController.java
@@ -5,47 +5,28 @@ import com.salesForce.service.EmpleadoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
 public class EmpleadoController {
     @Autowired
     EmpleadoService empleadoService;
 
-    // El sistema devuelve responses 200 en la ruta /enterprises con los siguientes verbos:  |GET|POST|
+    @PostMapping("/employees")
+    public void crearEmpleado (Empleado empleado){
+        empleadoService.crearEmpleado(empleado);
 
-    //Se puede crear un empleado segun post
-
-    @PostMapping("/user")
-    public List<Empleado> crearEmpleado (@RequestBody Empleado empleado){
-        empleadoService.crearEmpleado (empleado);
-        return empleadoService.consultarEmpleados ();
     }
-
-    //Se puede consultar la lista de empleados
-    @GetMapping("/user")
-    public List<Empleado> consultarEmpleados (){
-        List<Empleado> empleados = empleadoService.consultarEmpleados();
-        return empleados;
-    }
-
-    // El sistema devuelve responses 200 en la ruta /enterprises/[id] con los siguientes verbos: |GET|PATCH|DELETE|
-
-    @GetMapping("/user/{id_empleado}")
-    public  Empleado consultarEmpleado (@PathVariable ("id_empleado") Long id_Empleado) {
+    @GetMapping("/employees")
+    public  Empleado obtenerEmpleado (Long id_Empleado) {
         return empleadoService.consultarEmpleado (id_Empleado) ;
-    }
 
-    @DeleteMapping("/user/{id_empleado}")
-    private List<Empleado> eliminarEmpleado (@PathVariable ("id_empleado") Long id) {
-        empleadoService.borrarEmpleado(id);
-        return empleadoService.consultarEmpleados();
     }
+    @DeleteMapping("/employees/{id}")
+    private  void eliminarEmpleado (Long id) {
+           empleadoService.borrarEmpleado(Long id);
 
-    @PatchMapping("/user/{id_empleado}/{nombreEmpleado}")
-    public Empleado editarNombreEmpleado (@PathVariable ("id_empleado")Long id,@PathVariable ("nombreEmpleado") String nombreEmpleado ){
-        empleadoService.editarNombreEmpleado(id,nombreEmpleado);
-        return empleadoService.consultarEmpleado(id);
+    }
+    @PatchMapping("/employees") {
+        public
     }
 
 
