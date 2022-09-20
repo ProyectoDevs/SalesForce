db91d04c662125306f0197be6f9e9f7006508827
Fresneda777 <michael.fresneda@hotmail.com>
2022-09-20 16:05:03 -0500

    LoginSecurity

--- src/main/java/com/salesForce/controller/HomeController.java
+++ src/main/java/com/salesForce/controller/HomeController.java
@@ -4,6 +4,8 @@ package com.salesForce.controller;
 import com.salesForce.service.MovimientoDineroService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -18,7 +20,7 @@ public class HomeController {
     private MovimientoDineroService movimientoDineroService;
 
     @GetMapping("/")
-    public String verHome(Model model) {
+    public String verHome(Model model, @AuthenticationPrincipal OidcUser principal) {
         var movimientos = movimientoDineroService.getLstMovimientos();
         log.info("saludos desde Home");
         model.addAttribute("movimientos", movimientos);
